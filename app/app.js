9071239ef60c5bd5a99de93042cbd41682ff1918
Max <Max@driesfrs-MacBook-Pro-246.local>
2017-07-06 15:26:38 +0200

    filter on entities added

--- app/app.js
+++ app/app.js
@@ -1,34 +1,49 @@
 new Vue({
-	el:'#vue-app',
-	data: {
-		items: [],
-		output: [],
-		socialMediaFilters: [
-			{ name: 'Twitter', active: true },
-			{ name: 'Facebook', active: false }
-		],
-		dateFilters: [
-			{ year: 2017, active: true },
-			{ year: 2016, active: false }
-		],
-		url: 'https://osoc-2017-datascouts-akad1070.c9users.io/twitter/q/osoc',
-		newEntity: '',
-		entities: [],
-		vueIsWorking: 'Hurray, Vue is working!'
-	},
-	methods: {
-		fetchData: function () {
-		var xhr = new XMLHttpRequest()
-		var self = this
-		xhr.open('GET', 'https://osoc-2017-datascouts-akad1070.c9users.io/twitter/q/photo')
-		xhr.onload = function () {
-			console.log(xhr.responseText)
-			self.items = JSON.parse(xhr.responseText)
-		}
-		xhr.send()
-		},
-		test: function(){
-			newEntity='working';
-		}
-	}
+  el:'#vue-app',
+  data: {
+    items: [],
+    socialMediaFilters: [
+      {name: 'Twitter',
+      active: true},
+      {name: 'Facebook',
+      active: false}
+    ],
+    dateFilters: [
+      {year: 2017,
+      active: true},
+      {year: 2016,
+      active: false}
+    ],
+    url: 'https://osoc-2017-datascouts-akad1070.c9users.io/twitter/q/',
+    newEntity: '',
+    currentEntity: '',
+    entities: [],
+    vueIsWorking: 'Hurray, Vue is working!'
+  },
+  methods: {
+    fetchData: function () {
+      var xhr = new XMLHttpRequest()
+      var self = this
+      xhr.open('GET', self.url + self.currentEntity)
+      xhr.onload = function () {
+        console.log(xhr.responseText)
+        self.items = JSON.parse(xhr.responseText)
+
+      }
+      xhr.send()
+    },
+    addEntity: function (e) {
+      e.preventDefault()
+      this.currentEntity = this.newEntity
+      this.fetchData()
+      this.entities.push(this.newEntity)
+      this.newEntity=''
+    },
+    selectEntity: function (entity, e) {
+      e.preventDefault()
+      //console.log(entity)
+      this.currentEntity = entity
+      this.fetchData()
+    }
+  }
 })
