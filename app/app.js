1e3cafa99ebc7a3483796926db0747eb5be37c79
Maxime Van Driessche <van-driessche-maxime@hotmail.com>
2017-07-09 20:03:07 +0200

    waterfall working. display mockdata if no input

--- app/app.js
+++ app/app.js
@@ -15,28 +15,55 @@ new Vue({
       active: false}
     ],
     url: 'https://osoc-2017-datascouts-akad1070.c9users.io/twitter/q/',
+    mockDataTwitter: 'http://www.json-generator.com/api/json/get/ckwxgssyXm?indent=2',
     newEntity: '',
     currentEntity: '',
     entities: [],
+    waterfall: new Waterfall(),
+    limit: 20,
     vueIsWorking: 'Hurray, Vue is working!'
   },
   methods: {
     fetchData: function () {
       var xhr = new XMLHttpRequest()
       var self = this
-      xhr.open('GET', self.url + self.currentEntity)
+      var u = self.url + self.currentEntity + '?limit=' + self.limit
+      if(self.currentEntity=='mockdata'){
+        u = self.mockDataTwitter
+      }
+      xhr.open('GET', u)
       xhr.onload = function () {
         console.log(xhr.responseText)
         self.items = JSON.parse(xhr.responseText)
 
       }
       xhr.send()
+      xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4){
+              //Eventhough the data has finished loading, you have to wait a few
+              //milliseconds for vue to add the elements to the DOM (i think)
+              setTimeout(function () {
+                waterfall = new Waterfall()
+              }, 30);
+
+            }
+      }
     },
     addEntity: function (e) {
       e.preventDefault()
-      this.currentEntity = this.newEntity
+      if(this.newEntity==undefined || this.newEntity.length==0){
+        if(this.entities.indexOf('mockdata')==-1){
+          this.entities.push('mockdata')
+        }
+        this.currentEntity='mockdata'
+      }
+      else{
+        if(this.entities.indexOf(this.newEntity)==-1){
+          this.entities.push(this.newEntity)
+        }
+        this.currentEntity=this.newEntity
+      }
       this.fetchData()
-      this.entities.push(this.newEntity)
       this.newEntity=''
     },
     selectEntity: function (entity, e) {
