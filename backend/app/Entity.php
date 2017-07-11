40013684b9f1f0b05f535b6c18625b3c7866bbe2
A. David <akad@live.be>
2017-07-11 19:00:32 +0000

    CRUD on Entity : OK

--- backend/app/Entity.php
+++ backend/app/Entity.php
@@ -1,9 +1,19 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Entity extends Model {
+    
+    // use SoftDeletes;
 
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ["name", "image"];
 
     /**
