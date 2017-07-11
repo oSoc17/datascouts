ab7e2b39e608b7ed9079933ea4acee61de2d6a68
Bakary Potter <akad@live.be>
2017-07-11 02:11:27 +0000

    Add config files

---
+++ backend/config/abraham-twitteroauth.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+
+    /**
+     * To work with abraham/twitteroauth you'll need some credentials.
+     *
+     * If you don't have credentials yet, head over to https://apps.twitter.com/
+     */
+
+    'access_token' => env('TWITTER_ACCESS_TOKEN'),
+
+    'access_token_secret' => env('TWITTER_ACCESS_TOKEN_SECRET'),
+
+    'consumer_key' => env('TWITTER_CONSUMER_KEY'),
+
+    'consumer_secret' => env('TWITTER_CONSUMER_SECRET'),
+];
