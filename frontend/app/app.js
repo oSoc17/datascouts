52bbc2966854140872704823a075d6ab239caa86
mrvdries <van-driessche-maxime@hotmail.com>
2017-07-13 11:09:28 +0200

    Feature/36 implement crud handles (#59)
    * Basic functionality for CRUD for handles
    
    * finished both the entities & handles CRUD implementation
    
    * minor corrections
    
    * add entity bug fix
    
    * added uuid as identifier instead of url. loadHandles() bug fix

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,4 +1,4 @@
-new Vue({
+var vue = new Vue({
   el:'#vue-app',
   /*http: {
     root: '/root',
@@ -22,12 +22,45 @@ new Vue({
     ],
     url: 'https://osoc-2017-datascouts-backend-akad1070.c9users.io/api/v1',
     mockDataTwitter: 'http://www.json-generator.com/api/json/get/ckwxgssyXm?indent=2',
-    entities: ['osoc','tesla','spacex', 'apple'],
+    entities: [
+      {name: 'osoc',
+      url: 'osoc.com',
+      uuid: '1',
+      image: ''},
+      {name: 'tesla',
+      url: 'tesla.com',
+      uuid: '2',
+      image: ''},
+      {name: 'spacex',
+      url: 'spacex.com',
+      image: '',
+      uuid: '3'},
+
+    ],
     selectedEntities: [],
     searchEntity: '',
-    currentEntity: '',
-    handles: ['facebook/user1','facebook/user2','twitter/user'],
-    selected: false,
+    currentEntity: {
+      name: '',
+      url: '',
+      image: '',
+      uuid: '',
+    },
+    handles: [
+      {name: 'facebook/user1',
+      url: 'url1',
+      uuid:'1'},
+      {name: 'facebook/user2',
+      url: 'url2',
+      uuid:'2'}
+    ],
+    currentHandle: {
+      name: '',
+      url: ''
+    },
+    newName: '',
+    newUrl: '',
+    isEntitySelected: false,
+    handleSelected: false,
     waterfall: new Waterfall(200),
     waterfallIsCreated: false,
     isLoading: false,
@@ -63,71 +96,171 @@ new Vue({
       });
 
       //now the real magic happens
-      /*this.$http.get(this.url + '/entities', JSON.stringify(entities)).then(function (response) {
-          this.items = JSON.parse(response.data)
+      /*this.$http.post(this.url + '/providers/fetch', ).then(function (response) {
+          this.items = response.data
           //console.log(response)
         }, function (response) {
           console.log("Error Fail to get data")
       });*/
 
     },
-    addEntity: function (entity, e) {
-      e.preventDefault()
-      console.log(entity)
-      this.$http.post(this.url + '/entities', {"name" : entity}).then(function (response) {
-          console.log("Entity added")
+    loadEntities: function() {
+      this.$http.get(this.url + '/entities').then(function (response) {
+        this.entities = response.data
+        console.log("Entities loaded")
           //console.log(response)
+        }, function (response) {
+          console.log("Error Failed to get data")
+          console.log(response)
+      })
+    },
+    loadHandles: function(entity) {
+      this.$http.get(this.url + '/handles/' + entity.uuid).then(function (response) {
+        for(var i=0; i<response.data.length; i++){
+          this.handles[i] = response.data[i]
+        }
+        console.log("Handles loaded")
+          //console.log(response)
+        }, function (response) {
+          console.log("Error Fail to get data")
+          console.log(response)
+      })
+    },
+    addEntity: function (name, e) {
+      e.preventDefault()
+      this.$http.post(this.url + '/entities', {"name" : name}).then(function (response) {
+        this.loadEntities()
+        console.log("Entity added")
         }, function (response) {
           console.log("Error Failed to add entity")
       })
       this.searchEntity=""
-      this.loadEntities()
     },
     selectEntity: function (entity, e) {
       e.preventDefault()
       //console.log(entity)
-      if(!this.selected || this.currentEntity==entity){
-        this.selected = !this.selected
+      if(!this.isEntitySelected || this.currentEntity.uuid==entity.uuid){
+        this.isEntitySelected = !this.isEntitySelected
       }
-      this.currentEntity = entity
-      if(this.selected){
+      this.currentEntity.name = entity.name
+      this.currentEntity.url = entity.url
+      this.currentEntity.image = entity.image
+      this.currentEntity.uuid = entity.uuid
+      if(this.isEntitySelected){
         document.getElementById("sidenav_handles").style.marginLeft = "250px"
       }
       else{
         document.getElementById("sidenav_handles").style.marginLeft = "0px"
+        this.discardHandle()
+      }
+
+      this.loadHandles(entity)
+    },
+    confirmEditEntity: function(entity, e){
+      e.preventDefault()
+      var newName = prompt("Enter a new name for your entity", entity.name);
+      if(newName != null){
+        this.editEntity(entity, newName, e)
       }
     },
     editEntity: function(entity, newName, e) {
       e.preventDefault()
       console.log(entity)
-      this.$http.put(this.url + '/entities', {"id:" : entity, "name" : newName}).then(function (response) {
-          console.log("Entity added")
+      this.$http.put(this.url + '/entities/'+ entity.uuid,
+      {"name": newName}).then(function (response) {
+          console.log("Entity updated")
           //console.log(response)
         }, function (response) {
           console.log("Error Failed to edit entity")
       })
       this.loadEntities()
     },
+    confirmDeleteEntity: function(entity, e) {
+      if(confirm("Are you sure you want to delete this entity?") == true){
+        this.deleteEntity(entity, e)
+      }
+    },
     deleteEntity: function(entity, e) {
       e.preventDefault()
-      console.log(entity)
-      this.$http.delete(this.url + '/entities', {"name" : entity}).then(function (response) {
-          console.log("Entity added")
+      console.log(entity.name)
+      this.$http.delete(this.url + '/entities/' + entity.uuid).then(function (response) {
+          console.log("Entity deleted")
           //console.log(response)
         }, function (response) {
           console.log("Error Failed to delete entity")
       })
       this.loadEntities()
     },
-    loadEntities: function() {
-      this.$http.get(this.url + '/entities').then(function (response) {
-        this.entities = response.data.map(e => e.name);
-        console.log("Entity added")
+    promptAddHandle: function(entity, e){
+      e.preventDefault()
+      var name = prompt("Enter a name for your handle", "");
+      if(name != null){
+        var url = prompt("Enter a url for your handle", "");
+        if(url != null){
+          var handle = {"name" : name, "url" : url, "socialMedia": 'twitter'}
+          this.addHandle(entity, handle, e)
+        }
+      }
+    },
+    addHandle: function (entity, handle, e) {
+      e.preventDefault()
+      this.$http.post(this.url + '/handles/'+ entity.uuid, {"name" : handle.name, "url" : handle.uuid,
+    "socialMedia" : handle.socialMedia}).then(function (response) {
+        this.loadHandles()
+        console.log("Handle added")
+        }, function (response) {
+          console.log("Error Failed to add handle")
+      })
+      this.searchEntity=""
+    },
+    selectHandle: function(handle, e) {
+      e.preventDefault()
+      //console.log(entity)
+      if(!this.handleSelected || this.currentHandle.uuid==handle.uuid){
+        this.handleSelected = !this.handleSelected
+      }
+      this.currentHandle.name = handle.name
+      this.currentHandle.url = handle.url
+      this.currentHandle.uuid = handle.uuid
+      if(this.handleSelected){
+        document.getElementById("sidenav_action").style.marginLeft = "500px"
+      }
+      else{
+        document.getElementById("sidenav_action").style.marginLeft = "0px"
+      }
+    },
+    editHandle: function(handle, e) {
+      e.preventDefault()
+      console.log(handle.uuid)
+      this.$http.put(this.url + '/services/' + handle.uuid, {"name:" : handle.name, "url" : handle.url,
+    "socialMedia" : handle.socialMedia}).then(function (response) {
+          console.log("Handle updated")
           //console.log(response)
         }, function (response) {
-          console.log("Error Fail to get data")
-          console.log(response)
+          console.log("Error Failed to update handle")
       })
+      this.loadHandles(this.currentEntity)
+    },
+    confirmDeleteHandle: function(handle, e) {
+      if(confirm("Are you sure you want to delete this handle?") == true){
+        this.deleteEntity(handle, e)
+      }
+    },
+    deleteHandle: function(handle, e) {
+      e.preventDefault()
+      console.log(handle)
+      this.$http.delete(this.url + '/services'/ + handle.uuid).then(function (response) {
+          console.log("Handle deleted")
+          //console.log(response)
+        }, function (response) {
+          console.log("Error Failed to delete handle")
+      })
+      this.loadEntities()
+    },
+    discardHandle: function(e) {
+      this.handleSelected = !this.handleSelected
+      document.getElementById("sidenav_action").style.marginLeft = "0px"
+      this.currentHandle.uuid = ""
     },
     updateWaterfall: _.debounce(
         function() {
