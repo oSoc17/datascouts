b9620aea85253a236e9791ab2f68033e948791e7
Dakad <Dakad@users.noreply.github.com>
2017-07-15 10:21:40 +0200

    Feature/22 scheduler jobs fetch (#75)
    * Create jobs table in Db
    
    * Add DataScouts Command to fetch handler data
    
    * Add Twitter to handle twitter data
    
    * Alter Handle Model for the Scheduler part
    
    * Modify DataScoutsCommand && TwitterJob
    
    * Add scope in Handle Model
     
    * Pose the init. struct. for Job
    
    * Command to list all scheduler running

--- backend/app/Models/Handle.php
+++ backend/app/Models/Handle.php
@@ -1,15 +1,22 @@
 <?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Handle extends Model
 {
+    // use BaseModel;
 
+    protected $fillable = ["name", "url", "entity_id"];
 
-
-    protected $fillable = ["name", "url"];
-
-    protected $dates = [];
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'fetched_at'
+    ];
 
     public static $rules = [
         "name" => "string|required|min:3",
@@ -21,7 +28,31 @@ class Handle extends Model
     public $visible = [ ];
 
 
+    // Local Scope
 
+    // is Fetchable when is_Fetching === 0
+    public function scopeFetchable($query)
+    {
+        return $query
+                ->where('is_fetching', 0)
+                ->whereNotNull('service_id')
+                ->whereNotNull('provider_id');
+    }
+
+    // Check for the last fetch datetime in the last 5 min.
+    public function scopeIsOutDated($query)
+    {
+        return $query->where('fetched_at', '<=', Carbon::now()->subMinute(5));
+    }
+
+
+    
+
+    // Accessors & Mutators
+
+    public function setUrlAttribute($value){
+        $this->attributes['url'] = $this->getTable().'_'.str_slug($this->attributes['name']);
+    }
 
     // Relationships
 
