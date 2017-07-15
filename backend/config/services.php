ab0a6fdd7b4bac8b20012ef8c54fdcae2278c0b2
A. David <akad@live.be>
2017-07-15 22:11:12 +0200

    Add Sociale packge to handle oAuth.

---
+++ backend/config/services.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'github' => [
+        'client_id' => env('GITHUB_APP_ID'),
+        'client_secret' => env('GITHUB_APP_SECRET'),
+        'redirect' => env('GITHUB_APP_CALLBACK'),
+    ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_APP_ID'),
+        'client_secret' => env('FACEBOOK_APP_SECRET'),
+        'redirect' => env('FACEBOOK_APP_CALLBACK'),
+        'defaulft' => [
+            'public_profile',
+            'user_events',
+            'user_likes',
+            'user_posts',
+            'user_videos'
+        ]
+     ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_APP_ID'),
+        'client_secret' => env('TWITTER_APP_SECRET'),
+        'redirect' => env('TWITTER_APP_CALLBACK')
+    ]
+];
