4a94d84f5b786d4c46d84d5a58ddf53fcefd2c00
Bakary Potter <akad@live.be>
2017-07-25 08:10:57 +0000

    RE-Rewrite storageService

--- frontend/src/utils/storageService.js
+++ frontend/src/utils/storageService.js
@@ -13,7 +13,7 @@ const KEY_FOR_ACTIVE_ENTITIES = 'ACTIVE_ENTITIES';
 
 const KEY_FOR_LAST_HANDLE    = 'LAST_HANDLES_FOR_ENTITY_';
 const KEY_FOR_ACTIVE_HANDLES = 'ACTIVE_HANDLE_FOR_ENTITY_';
-const KEY_FOR_CURRENT_HANDLE = 'CURRENT_HANDLE_FOR_ENTITY_';
+const KEY_FOR_CURRENT_HANDLE = 'CURRENT_HANDLES';
 
 
  /**
@@ -25,44 +25,67 @@ const KEY_FOR_CURRENT_HANDLE = 'CURRENT_HANDLE_FOR_ENTITY_';
  * Same process to cache/retrieve some object into/from the localStorage
  *
  */
-const marshall = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+const marshall = (key, value) => localStorage.setItem(key, JSON.stringify(value))
 
-const unmarshall = (key) => JSON.parse(localStorage.getItem(key));
+const unmarshall = (key) => JSON.parse(localStorage.getItem(key))
+
+const remove = (key) => localStorage.removeItem(key)
 
 
+
+ /**
+  * ENTITIES 
+  *
+  */
 
 export const saveCurrentEntity = v => marshall(KEY_FOR_CURRENT_ENTITY, v)
 
 export const getCurrentEntity = _ => unmarshall(KEY_FOR_CURRENT_ENTITY)
 
+export const removeCurrentEntity = _ => remove(KEY_FOR_CURRENT_ENTITY)
+
+
 export const setLastEntity = v => marshall(KEY_FOR_LAST_ENTITY, v)
 
 export const getLastEntity = _ => unmarshall(KEY_FOR_LAST_ENTITY)
+
+export const removeLastEntity = _ => remove(KEY_FOR_LAST_ENTITY)
+
 
 export const saveActiveEntities = l => marshall(KEY_FOR_ACTIVE_ENTITIES, l)
 
 export const getActiveEntities = _ => unmarshall(KEY_FOR_ACTIVE_ENTITIES) || []
 
+export const removeActivEntity = k => remove(KEY_FOR_ACTIVE_ENTITIES)
+
 
  /**
   * HANDLES 
-  *
+  * The entity ID is required for the last/actives Handle function.
   */
 
-export const saveCurrentHandle = (id, handle) => {
-    return marshall(KEY_FOR_CURRENT_HANDLE + id, handle)
+export const saveCurrentHandle = (handle) => {
+    return marshall(KEY_FOR_CURRENT_HANDLE, handle)
 }
 
 export const getCurrentHandle = _ => unmarshall(KEY_FOR_CURRENT_HANDLE)
 
+export const removeCurrentHandle = _ => unmarshall(KEY_FOR_CURRENT_HANDLE)
+
+
 export const setLastHandle = (id, h) => marshall(KEY_FOR_LAST_HANDLE + id, h)
 
-export const getLastHandle = id => unmarshall(KEY_FOR_LAST_HANDLE)
+export const getLastHandle = id => unmarshall(KEY_FOR_LAST_HANDLE + id)
+
+export const removeLastHandle = id => remove(KEY_FOR_LAST_HANDLE + id)
+
 
 export const saveActiveHandles = (id, list) => {
     return marshall(KEY_FOR_ACTIVE_HANDLES + id, list)
 }
 
 export const getActiveHandles = id => unmarshall(KEY_FOR_ACTIVE_HANDLES + id) || []
+
+export const removeActiveHandles = id => remove(KEY_FOR_ACTIVE_HANDLES + id)
 
 
