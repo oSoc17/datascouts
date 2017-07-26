5ee86a555977bbc7b20aba8dc5b702e7b05c4167
Ismail Kutlu <ismail.kutlu@student.arteveldehs.be>
2017-07-26 15:17:03 +0200

    Implemented vimeo feeds (API doesn't work)

--- frontend/src/components/results/WaterfallDisplay.vue
+++ frontend/src/components/results/WaterfallDisplay.vue
@@ -149,6 +149,49 @@
 						</div>
 					</template>
 
+					<!--VIMEO-->
+					<template v-if="handle.service_name === 'vimeo'">
+					<div class="wf-box vimeo">
+						<!-- BODY -->
+						<div class="body">
+							<p class="title">
+								{{ handle.title }}
+							</p>
+							<p class="description">
+								{{ handle.description }}
+							</p>
+						</div>
+
+						<!-- THUMBNAIL -->
+						<div style="position: relative;">
+							<img v-bind:src="handle.thumbnail" alt="" class="media">
+							<a href="" class="play-button">
+								<i class="fa fa-youtube-play"></i>
+							</a>
+						</div>
+						
+						<!-- VIDEO-IFRAME -->
+						<!-- <figure class="content-media content-media--video" id="featured-media">
+							<iframe class="content-media__object media" id="featured-video" v-bind:src='handle.link + "?enablejsapi=1&rel=0&showinfo=0&controls=0"' frameborder="0"></iframe>
+						</figure> -->
+
+						<!-- METADATA - likes/comments/views -->
+						<div class="metadata_1">
+							<i class="fa fa-views"></i> 50,000
+							<a v-bind:href="'https://www.youtube.com/watch?v=' + handle.id" target="_blank"><i class="fa fa-external-link"></i></a>
+						</div>
+						<div class="metadata_2">
+							<div class="image_avatar">
+								<img src="handle.user.profile_image_url_https" alt="" class="avatar">
+							</div>
+							<div class="name">{{handle.channel}}</div>
+								<div class="social_media">
+									<i class="fa fa-vimeo"></i>
+								</div>
+							</div>
+						</div>
+					</template>
+
 				</template>
 			</template>
 		</div>
