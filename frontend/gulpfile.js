891c916c8898fdefd096881409804f1c436cca2a
Ismail Kutlu <ismail.kutlu@student.arteveldehs.be>
2017-08-02 12:20:14 +0200

    Added minify css + final changes

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,16 +2,23 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
  
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch('./sass/**/*.scss', ['sass']);
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./css'));
 });
