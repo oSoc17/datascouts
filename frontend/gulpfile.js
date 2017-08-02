cbfeed2e038290019af8aa74a6bd810b090f4a97
Dakad <akad@live.be>
2017-08-02 12:06:56 +0000

    Simply add outputStyle:compressed instead of another package

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,23 +2,14 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-let cleanCSS = require('gulp-clean-css');
- 
+
 gulp.task('sass', () => {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', () => {
   gulp.watch('./sass/**/*.scss', ['sass']);
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('minify-css', () => {
-  return gulp.src('css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
-});
