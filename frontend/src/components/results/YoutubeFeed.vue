7eaac1b2c98a7ea32352743a18cf9f09e6a08054
Dakad <akad@live.be>
2017-08-05 15:07:41 +0000

    Use vue-youtube-embeed for the YoutubeFeed

--- frontend/src/components/results/YoutubeFeed.vue
+++ frontend/src/components/results/YoutubeFeed.vue
@@ -26,13 +26,16 @@
     </div>
 
     <!-- VIDEO-IFRAME -->
-    <figure class="content-media content-media--video featured-media" 
-            v-if="feed.type == 'video' && isLoaded"
-    >
-      <iframe class="content-media_object media featured-video" 
-              frameborder="0"
-              v-bind:src="feed.link + '?enablejsapi=1&autoplay=1&controls=0&rel=1&showinfo=0'"
-      > </iframe>
+    <figure class="content-media"  ref="media">
+      <youtube  v-if="feed.type == 'video' && isLoaded" 
+                ref="ytb_vid" 
+                :videoId="feed.id"
+                :player-vars="{ autoplay: 1, controls: 0,  showinfo: 0, rel: 0 }" 
+                @ready="ready" 
+                @playing="playing" 
+                @error="error"
+      ></youtube>
+
     </figure>
 
     <!-- METADATA - likes/comments/views -->
@@ -52,19 +55,33 @@
 </template>
 
 <script>
-  import { bus } from '../../main'
+  import _throttle from 'lodash.throttle'
+  
+  import Vue from 'vue'
+  import VueYouTubeEmbed from 'vue-youtube-embed'
+
+  import { bus, app } from '../../main'
+  
+  
+  // app.use(VueYouTubeEmbed, { global: false })
+
 
 	export default {
 		props: ['feed', 'keyword', 'active'],
-		components: {
-		},
+		components: {VueYouTubeEmbed },
 		data() {
 			return {
         isLoaded : false,
+        ids : {
+          media : 'media-'+this.feed.id,
+          video : 'video-'+this.feed.id,
+        },
+        player : {}
 			}
 		},
 		created() { },
 		mounted() { },
+		watch : { },
 		computed : {
    	  hasKeyword: function(item){
     		if(this.keyword){
@@ -75,9 +92,96 @@
     		}
     		// No keyword selected
   		  return true
-    	}
+    	},
+    	generateIframeSrc : function(){
+    	  let link = this.feed.link 
+    	  link += '?enablejsapi=1&autoplay=1&controls=0&rel=1&showinfo=0'
+    	  return link
+    	},
 		},
 		methods: {
+		
+		  onPlayerStateChange : function(e) {
+        console.log('Yep', e.data);
+        switch (e.data) {
+            case YT.PlayerState.PLAYING:
+                this.player.$featuredVideo.removeClass("is-paused");
+                this.player.$featuredVideo.toggleClass("is-playing");
+                break;
+
+            case YT.PlayerState.PAUSED:
+                // this.player.$featuredVideo.removeClass("is-playing");
+                // this.player.$featuredVideo.toggleClass("is-paused");
+                break;
+
+            case YT.PlayerState.ENDED:
+                this.player.$featuredVideo.removeClass("is-playing", "is-paused");
+                break;
+
+            default:
+                break;
+        }
+    },
+		  
+		  initStickyFloat : function (e){
+		    console.log('? isLoaded ?');
+		    
+		    if(!this.isLoaded) return
+
+        const $featuredMedia = this.$el.querySelector('#'+this.ids.media); // Container.
+        const $featuredVideo = $featuredMedia.querySelector('#'+this.ids.video); // Actual Video.
+        
+        console.log($featuredMedia, '#'+this.ids.video ,$featuredVideo, this.$refs.video);
+        
+        // this.player = {
+        //   top : $featuredMedia.offset().top,
+        //   offset : Math.floor(top + ($featuredMedia.outerHeight() / 2)),
+        //   $featuredMedia, $featuredVideo
+        // };
+        
+        // window.onYouTubeIframeAPIReady = function() {
+        //   player = new YT.Player("featured-video-2", {
+        //       events: {
+        //           "onStateChange": this.onPlayerStateChange
+        //       }
+        //   });
+        // };
+  
+        // $window
+        //   .on("resize", _throttle(function() {
+        //       top = this.player.$featuredMedia.offset().top;
+        //       offset = Math.floor(top + (this.player.$featuredMedia.outerHeight() / 2));
+        //   }, 150))
+        //   .on("scroll", _throttle(function() {
+        //       this.player.$featuredVideo.toggleClass("is-sticky",
+        //           $window.scrollTop() > offset && this.player.$featuredVideo.hasClass("is-playing")
+        //       );
+        //   }, 350)); 
+		  },
+		  
+		  ready (player) {
+		    console.log(this.feed.id + ' => READY');
+        this.player = player
+        
+      },
+      playing (player) {
+		    console.log(this.feed.id + ' => PLAYING');
+        // The player is playing a video.
+      },
+
+      stop () {
+		    console.log(this.feed.id + ' => STOPPED');
+        this.player.stopVideo()
+      },
+      pause () {
+		    console.log(this.feed.id + ' => PAUSED');
+        this.player.pauseVideo()
+      },
+      
+      error (err) {
+		    console.log(this.feed.id + ' => ERROR', err);
+        
+      },
 		}
 
 	}
@@ -85,25 +189,50 @@
 </script>
 
 <style>
-
-.featured-media iframe {
-  top: 0;
-  left: 0;
-  height:100%;
-  width: 100%;
-}
-
-
-.play-button {
-  cursor : pointer;
-}
-
-.metadata_2 .name a {
-  color: #515151;
-  text-decoration: none;
-}
-
-.social_media a .fa {
-  color : #FFF;
-}
+  .content-media iframe {
+    top: 0;
+    left: 0;
+    height:100%;
+    width: 100%;
+  }
+  
+  
+  .play-button {
+    cursor : pointer;
+  }
+  
+  .metadata_2 .name a {
+    color: #515151;
+    text-decoration: none;
+  }
+  
+  .social_media a .fa {
+    color : #FFF;
+  }
+  
+  .content-media-video {
+      transition: width .2s ease-in-out, height .2s ease-in-out, transform .38s ease-in-out;
+  }
+  /** Use .sticky */
+  
+  
+  .is-sticky {
+      position: fixed;
+      top: 15px;
+      right: 0;
+      min-width: 480px;
+      min-height: 270pc;
+  }
+  
+  @media screen and (min-width: 1120px) {
+      .content-media-video .is-sticky {
+          transform: translateX(-30%);
+      }
+  }
+  
+  @media screen and (min-width: 1300px) {
+      .content-media-video .is-sticky {
+          transform: translateX(-75%);
+      }
+  }
 </style>
